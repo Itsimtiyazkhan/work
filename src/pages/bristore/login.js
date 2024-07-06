4655ead116180f965459840b732f7ddd96608f8f
Imtiyaz-BriBooks <imtiyaz@bribooks.com>
2024-07-06 14:02:24 +0530

    file and folder

---
+++ src/pages/bristore/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "@/components/bribooksstore/head/Navbar";
+import Footer from "@/components/bribooksstore/footer/Footer";
+import Login from "@/components/bribooksstore/login/login";
+const login = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default login;
